e2a5b5a6c40d429eff20eb0a5c951cc0cea2ff7a
xuechong <xuechong87@gmail.com>
2013-04-10 17:36:35 +0800

    2.5 fin

---
+++ src/main/chapter2/Test2_5.java
@@ -0,0 +1,23 @@
+package chapter2;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.junit.Test;
+
+public class Test2_5 {
+	
+	
+	
+	@Test
+	public void testNullInput() throws IOException{
+		OutputStream out = new OutputStream() {
+			@Override
+			public void write(int b) throws IOException {
+				System.out.println(b);
+			}
+		};
+		byte[] input = null;
+		out.write(input);
+	}
+}
