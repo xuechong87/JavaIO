80b63c7ff5532ad245668716de0781137fcd3472
xuechong <xuechong87@gmail.com>
2013-04-17 17:25:28 +0800

    4.1 half

---
+++ src/main/chapter3/StreamCopier.java
@@ -0,0 +1,27 @@
+package chapter3;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class StreamCopier {
+	public static void copy(InputStream in,OutputStream out) throws IOException{
+		byte[] buffer = new byte[1024];
+		while(true){
+			int bytesRead = in.read(buffer);
+			if(bytesRead==-1){
+				break;
+			}
+			out.write(buffer,0,bytesRead);
+			//out.write(buffer); if you do this ,thousand '0's will be write
+		}
+	}
+	
+	public static void main(String[] args) {
+		try {
+			copy(System.in, System.err);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
