af611a52c9bb1fac8d47a75010dff1f47417679d
xuechong87 <xuechong87@gmail.com>
2013-07-25 21:10:18 +0800

    16.1 start

---
+++ src/main/chapter16/DataStuffer.java
@@ -0,0 +1,50 @@
+package chapter16;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class DataStuffer {
+	private static byte[] data = new byte[256];
+
+	public static void main(String[] args) throws IOException {
+		int port = 9000;
+		for (int i = 0; i < data.length; i++)
+			data[i] = (byte) i;
+		ServerSocket server = new ServerSocket(port);
+		while (true) {
+			Socket socket = server.accept();
+			Thread stuffer = new StuffThread(socket);
+			stuffer.start();
+		}
+	}
+
+	private static class StuffThread extends Thread {
+		private Socket socket;
+
+		public StuffThread(Socket socket) {
+			this.socket = socket;
+		}
+
+		public void run() {
+			try {
+				OutputStream out = new BufferedOutputStream(
+						socket.getOutputStream());
+				while (!socket.isClosed()) {
+					out.write(data);
+				}
+			} catch (IOException ex) {
+				if (!socket.isClosed()) {
+					try {
+						socket.close();
+					} catch (IOException e) {
+						// Oh well. We tried.
+					}
+				}
+			}
+		}
+	}
+
+}
